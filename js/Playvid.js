a543cb0ae60c35a0b25e35c40f509d7816d6ccdb
OSMAR SF <71989728+OSMAR2910@users.noreply.github.com>
2022-12-20 11:44:42 -0800

    Add files via upload

--- js/Playvid.js
+++ js/Playvid.js
@@ -1,12 +1,13 @@
 var fondo = document.getElementById("vid");
 var sound = document.getElementById("aud");
 function playPause() {
-    if(fondo.paused)
-          fondo.play();
-          else
-          fondo.pause();
     if(sound.paused)
           sound.play();
           else
           sound.pause();
+    if(fondo.paused)
+          fondo.play();
+          else
+          fondo.pause();
+          
 }
