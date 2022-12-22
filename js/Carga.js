f29678f1583784b140e9a1e9ce70d953edb40dc2
OSMAR SF <71989728+OSMAR2910@users.noreply.github.com>
2022-12-21 22:00:14 -0800

    Delete js directory

--- js/Carga.js
+++
@@ -1,6 +0,0 @@
-//alert('hello world');
-window.onload = function() {
-    var contenedor = document.getElementById('logo_load');
-    contenedor.style.visibility = 'hidden';
-    contenedor.style.opacity = '0';
-}
